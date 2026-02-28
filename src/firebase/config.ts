d4a9750e2b459e31a1f43cbf3b59518ce909d802
John Michael Angelo B. Soriano <johnmichaelangelo.soriano@neu.edu.ph>
2026-02-28 11:29:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3230754876-e6aa7",
+  "appId": "1:1013208992651:web:607064f0d530353e58fd8e",
+  "apiKey": "AIzaSyB22NweLFgBu00tZKN_sHWsKyxFfyRimIM",
+  "authDomain": "studio-3230754876-e6aa7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1013208992651"
+};
